7174659ddc7dc140080ba50743654602163e51c3
Josh Glover <jmglov@jmglov.net>
2024-11-04 08:44:24 +0100

    Display supported formats

--- public/cljs/transcribble-fe.cljs
+++ public/cljs/transcribble-fe.cljs
@@ -1,3 +1,29 @@
-(ns transcribble.fe)
+(ns transcribble.fe
+  (:require [clojure.string :as str]
+            [transcribble.dom :as dom]
+            [transcribble.formats :as formats]
+            [transcribble.util :as util :refer [log]]))
 
-(js/console.log "Hi")
+(defn formats->str [formats]
+  (str "Your browser supports "
+       (str/join " / " (:audio formats))
+       " and "
+       (str/join " / " (:video formats))
+       " video files."))
+
+(defn update-supported-formats! []
+  (let [formats (formats/supported-formats)]
+    (log "Supported formats:" formats)
+    (dom/set-html! "#formats"
+                   (formats->str formats))))
+
+(defn load-ui! []
+  (update-supported-formats!))
+
+(comment
+
+  (load-ui!)
+
+  (dom/get-el "input")
+
+  )
