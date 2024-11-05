1da37010899cc0990bb1bd01a535104b83669c25
Josh Glover <jmglov@jmglov.net>
2024-11-05 08:18:22 +0100

    Save filename on selection

--- public/cljs/transcribble-fe.cljs
+++ public/cljs/transcribble-fe.cljs
@@ -2,6 +2,8 @@
   (:require [clojure.string :as str]
             [transcribble.dom :as dom]
             [transcribble.formats :as formats]
+            [transcribble.listeners :as listeners]
+            [transcribble.storage :as storage]
             [transcribble.util :as util :refer [log]]))
 
 (defn formats->str [formats]
@@ -17,13 +19,49 @@
     (dom/set-html! "#formats"
                    (formats->str formats))))
 
-(defn load-ui! []
-  (update-supported-formats!))
+(defn select-file [ev]
+  (let [el (.-target ev)
+        selected-file (first (.-files el))]
+    (log "Handling event" {:event ev, :file selected-file})
+    (storage/set-item! :last-file (.-name selected-file))))
+
+(defn add-file-input-listeners! [listeners]
+  (->> [["change" select-file]]
+       (map (partial apply listeners/add-event-listener! "input#file-picker"))
+       (concat listeners)))
+
+(defn load-ui!
+  ([]
+   (load-ui! []))
+  ([listeners]
+   (update-supported-formats!)
+   (->> listeners
+        (add-file-input-listeners!))))
 
 (comment
 
-  (load-ui!)
+  (def ls [])
 
-  (dom/get-el "input")
+  (def ls (load-ui! ls))
+
+  ls
+
+  (do
+    (def ls
+      (->> ls
+           (listeners/remove-event-listener! "input#file-picker" "change")))
+    ls)
+  ;; => ()
+
+  (storage/get-all)
+  ;; => ()
+  ;; => ({:key :last-file, :value "Upstream-Johann_Hari.mp3", :timestamp 1730790689960, :index 0})
+  ;; => ()
+  ;; => ({:key "_", :value nil, :timestamp 1730784759185, :index 0} {:key :last-file, :value "CleanShot 2022-11-17 at 13.08.45.mp4", :timestamp 1730790293529, :index 1})
+
+  (storage/remove-all!)
+  ;; => nil
+  ;; => nil
+
 
   )
