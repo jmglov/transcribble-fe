316833051788b80dcb719d10770f8dab41c2f046
Josh Glover <jmglov@jmglov.net>
2024-11-09 10:48:14 +0100

    dom: add add-class! and remove-class!

--- public/cljs/dom.cljs
+++ public/cljs/dom.cljs
@@ -1,10 +1,24 @@
 (ns transcribble.dom)
 
 (defn get-el
-  ([sel]
-   (get-el js/document sel))
+  ([sel-or-el]
+   (if (string? sel-or-el)
+     (get-el js/document sel-or-el)
+     sel-or-el))
   ([parent sel]
    (.querySelector parent sel)))
+
+(defn add-class!
+  ([sel-or-el cls]
+   (-> (get-el sel-or-el) (.-classList) (.add cls)))
+  ([parent sel cls]
+   (add-class! (get-el parent sel) cls)))
+
+(defn remove-class!
+  ([sel-or-el cls]
+   (-> (get-el sel-or-el) (.-classList) (.remove cls)))
+  ([parent sel cls]
+   (remove-class! (get-el parent sel) cls)))
 
 (defn set-html!
   ([sel-or-el html]
